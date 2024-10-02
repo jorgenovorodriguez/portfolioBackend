47e25708a8157735668f7c1393c1040b091f0639
HACKER <jorge.novo.rodriguez.93@gmail.com>
2024-10-03 01:51:34 +0200

    Actualizar consulta

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const config = require('./config');
 const app = require('./app');
 
-const PORT = config.app.port || 4000;
+const PORT = config.app.port;
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
